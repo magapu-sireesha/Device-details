b59d3dba0a62cb1fa18a4d839c35c9172ca6e40d
magapu-sireesha <sireesha.magapu2000@gmail.com>
2024-11-27 16:12:10 +0530

    Delete src directory

--- src/main/java/com/sample/device_details/repository/DetailsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sample.device_details.repository;
-
-import com.sample.device_details.entity.DeviceDetails;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DetailsRepository extends JpaRepository<DeviceDetails, Long> {
-}
